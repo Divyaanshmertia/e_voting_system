c8e708e91b839c291942deed74531ca3d1365307
Divyaansh Mertia <divyaanshmertia@gmail.com>
2023-11-23 01:20:29 +0530

    initial commit

--- app/static/script1.js
+++ app/static/script1.js
@@ -1,16 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
+ddocument.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM fully loaded and parsed');
     initializeFaceRecognition();
 });
 
-let recognitionActive = false;
-let recognitionStartTime = null;
-
 async function initializeFaceRecognition() {
+    console.log('Initializing facial recognition');
     const video = document.getElementById('video');
     const userId = 1; // Replace with actual user ID
     const imageUrls = await fetchUserImages(userId);
 
-    // Load face-api.js models
     await Promise.all([
         faceapi.nets.tinyFaceDetector.loadFromUri('/static/models'),
         faceapi.nets.faceLandmark68Net.loadFromUri('/static/models'),
@@ -18,7 +16,6 @@ async function initializeFaceRecognition() {
         faceapi.nets.ssdMobilenetv1.loadFromUri('/static/models'),
     ]);
 
-    // Process and label the user's face descriptors
     const labeledFaceDescriptors = await Promise.all(imageUrls.map(async url => {
         const img = await faceapi.fetchImage(url);
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
@@ -28,30 +25,32 @@ async function initializeFaceRecognition() {
     const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
     startVideo(video, faceMatcher);
 }
+
 async function fetchUserImages(userId) {
     try {
         const response = await fetch(`/api/users/user/${userId}/images`);
-
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        const data = await response.json();
-        return data.images; // Array of image URLs
+        return await response.json().images;
     } catch (error) {
         console.error("Could not fetch user images:", error);
+        return [];
     }
 }
 
 async function startVideo(video, faceMatcher) {
-    // Initialize webcam
-    navigator.getUserMedia({ video: {} }, stream => {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
         video.srcObject = stream;
-    }, err => console.error(err));
+    } catch (err) {
+        console.error('Error accessing webcam:', err);
+        return;
+    }
 
     video.addEventListener('play', () => {
-        // Set up canvas for facial recognition
         const canvas = faceapi.createCanvasFromMedia(video);
-        document.body.append(canvas);
+        document.body.appendChild(canvas);
         const displaySize = { width: video.width, height: video.height };
         faceapi.matchDimensions(canvas, displaySize);
 
@@ -61,32 +60,27 @@ async function startVideo(video, faceMatcher) {
                 .withFaceDescriptors();
             const resizedDetections = faceapi.resizeResults(detections, displaySize);
             canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-            faceapi.draw.drawDetections(canvas, resizedDetections);
-            faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-
-            // Check each detection and match faces
             detections.forEach(detection => {
                 const bestMatch = faceMatcher.findBestMatch(detection.descriptor);
-                if (bestMatch.label !== 'unknown' && bestMatch.distance < 0.5) {
-                    handleSuccessfulRecognition();
-                } else {
-                    resetRecognition();
+                if (bestMatch.label !== 'unknown' && bestMatch.distance < 0.4) {
+                    if (!recognitionActive) {
+                        recognitionActive = true;
+                        recognitionStartTime = new Date();
+                    } else {
+                        const currentTime = new Date();
+                        if (currentTime - recognitionStartTime >= 5000) {
+                            enableVoting();
+                        }
+                    }
                 }
             });
+            faceapi.draw.drawDetections(canvas, resizedDetections);
+            faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
         }, 100);
     });
 }
 
-function handleSuccessfulRecognition() {
-    if (!recognitionActive) {
-        recognitionActive = true;
-        recognitionStartTime = new Date();
-    } else if (new Date() - recognitionStartTime > 5000) { // 10 seconds
-        window.location.href = '/vote/vote'; // Redirect to the voting page
-    }
-}
-
-function resetRecognition() {
-    recognitionActive = false;
-    recognitionStartTime = null;
+function enableVoting() {
+    const voteButtons = document.querySelectorAll('.btn.btn-primary');
+    voteButtons.forEach(button => button.disabled = false);
 }
